58552eaaaee8d136eda24a4502262f6387e16ffc
Felipe Montealegre-Mora <felimomouni@gmail.com>
2025-05-16 07:08:56 -0700

    imports in __init__

--- myst-refsort/src/myst_refsort/__init__.py
+++ myst-refsort/src/myst_refsort/__init__.py
@@ -1,4 +1,5 @@
-from .target_dir import get_latest_modified_dir
+from .add_bibresources import add_bibresources
 from .bib_file_changes import bib_file_changes
-from .rm_bbl_aux import rm_bbl_aux
 from .biblatex_commands import use_biblatex, print_bibliography, citep_to_parencite
+from .rm_bbl_aux import rm_bbl_aux
+from .target_dir import get_latest_modified_dir
